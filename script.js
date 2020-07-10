cb6710400b8c2c6fb5b917cbaab7ad08a1f96695
Josh <jdanielson04@gmail.com>
2020-07-10 12:12:03 -0700

    added basic local storage and pulling said data while creating cards on page load

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-let myLibrary = [];
+
+/*var mainDiv = document.getElementById("libraryCards");
+var resetBtn = document.createElement("button");
+resetBtn.addEventListener("click", function() {
+  localStorage.clear();
+  myLibrary = [];
+})
+mainDiv.appendChild(resetBtn);*/
+
+var myLibrary = [];
+
+
+
+console.log("checking method");
+pullStorage();
+
+//Get form submit
+document.getElementById("submit").addEventListener("click", () => {
+  addBookToLibrary();
+  newBookModal.style.display = "none";
+  document.getElementById("newBook").reset();
+  localStorage.setItem('savedLibrary', JSON.stringify(myLibrary));
+});
+
+//FUNCTIONS
+
+function pullStorage(){
+  if(!localStorage.getItem('savedLibrary')) {
+    console.log("found nothing");
+  }
+  else 
+  { 
+    console.log("found something");
+    retrievedObject = localStorage.getItem('savedLibrary');
+    myLibrary = JSON.parse(retrievedObject);
+    myLibrary.forEach(generateBookCard);
+  }
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,24 +56,20 @@ function addBookToLibrary(){
   return false;
 }
 
-
-//Get form submit
-document.getElementById("submit").addEventListener("click", () => {
-  addBookToLibrary();
-  newBookModal.style.display = "none";
-  document.getElementById("newBook").reset();
-});
-
 function removeBook(i) {
-  //get book info
-  //remove from array
-  //refresh cards
-  console.log(i)
-  console.log(i.title)
   var deletedBook = document.getElementById("" + i.title.split(" ").join("").toLowerCase());
   deletedBook.remove();
   myLibrary.splice(myLibrary.indexOf(i), 1);
-  console.log(myLibrary);
+}
+
+function readBook(i) {
+  i.read = true;
+  var elementParent = document.getElementById("readBtn").parentNode;
+  var elementButton = document.getElementById("readBtn");
+  elementButton.remove();
+  const bookFinished = document.createElement("p");
+  bookFinished.textContent = "Finished";
+  elementParent.appendChild(bookFinished);
 }
 
  function generateBookCard(book) {
@@ -75,12 +108,20 @@ function removeBook(i) {
     bookFinished.textContent = "Finished";
     cardBody.appendChild(bookFinished);
   }
+  else {
+    const readBtn = document.createElement("button");
+    readBtn.setAttribute("id", "readBtn");
+    readBtn.innerHTML = "Finished?";
+    readBtn.addEventListener("click", () => {
+      readBook(book);
+    })
+    cardBody.appendChild(readBtn);
+  }
 
   card.appendChild(cardBody)
   document.getElementById("libraryCards").appendChild(card);
   }
  }
-
 
 //modal popup
 var newBookModal = document.getElementById("newBookModal");
@@ -103,10 +144,11 @@ window.onclick = function(event) {
   }
 }
 
-
-/*Checklist of features
-New Book Button
-Form For New Book Data
-Add "Cards" for Book Data
-Store Data Locally
-*/
+/*features to work on
+refuse book if in array
+make only cardContainer scrollable
+make pretty{
+  font size
+  red border around read cards?
+  card sizses and spacing
+}*/
